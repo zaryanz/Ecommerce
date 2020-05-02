d96a574193a8e16fc39608561814cbbcf94e5c26
zaryanz <aryanx404@gmail.com>
2020-05-02 20:11:12 +0530

    Basic server and material ui client setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../theme";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              Ecommerce
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default Header;
---
+++ client/src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { indigo, green } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: green,
+  },
+});
+
+export default theme;
